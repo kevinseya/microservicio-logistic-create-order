12b4f84626f24a1930a83061af5ecc0c2ba36a16
lunakenya <luna28kenya@gmail.com>
2025-02-10 13:26:27 -0500

    feat: ✨ Update the file dbConfig.js

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 // Database connection setup using environment variables
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: 'mariadb',
 });
 
 sequelize.authenticate()
